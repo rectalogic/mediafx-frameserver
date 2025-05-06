4bfc14622a8bf565baa183e5c236d98739b5b433
Andrew Wason <rectalogic@rectalogic.com>
2025-05-06 16:41:53 -0400

    drop internal module

--- frei0r/mediafx/src/lib.rs
+++ frei0r/mediafx/src/lib.rs
@@ -1,4 +1,3 @@
-// use frameserver::server;
 use std::{ffi::CString, marker::PhantomData};
 
 pub use frei0r_rs;
@@ -22,53 +21,49 @@ pub struct FrameServerPlugin<T> {
     _phantom: PhantomData<T>,
 }
 
-mod plugin {
-    use super::*;
+trait PluginDefault {
+    fn info() -> frei0r_rs::PluginInfo;
+}
 
-    pub(super) trait PluginDefault {
-        fn info() -> frei0r_rs::PluginInfo;
+impl<T> frei0r_rs::Plugin for FrameServerPlugin<T>
+where
+    FrameServerPlugin<T>: PluginDefault,
+{
+    fn info() -> frei0r_rs::PluginInfo {
+        <FrameServerPlugin<T> as PluginDefault>::info()
     }
 
-    impl<T> frei0r_rs::Plugin for super::FrameServerPlugin<T>
-    where
-        super::FrameServerPlugin<T>: PluginDefault,
-    {
-        fn info() -> frei0r_rs::PluginInfo {
-            <super::FrameServerPlugin<T> as PluginDefault>::info()
+    fn new(width: usize, height: usize) -> Self {
+        Self {
+            width: width as u32,
+            height: height as u32,
+            client_path: c"".to_owned(),
+            frame_server: None,
+            _phantom: PhantomData,
         }
+    }
 
-        fn new(width: usize, height: usize) -> Self {
-            Self {
-                width: width as u32,
-                height: height as u32,
-                client_path: c"".to_owned(),
-                frame_server: None,
-                _phantom: PhantomData,
-            }
-        }
+    fn update(
+        &self,
+        _time: f64,
+        _width: usize,
+        _height: usize,
+        _inframe: &[u32],
+        _outframe: &mut [u32],
+    ) {
+    }
 
-        fn update(
-            &self,
-            _time: f64,
-            _width: usize,
-            _height: usize,
-            _inframe: &[u32],
-            _outframe: &mut [u32],
-        ) {
-        }
-
-        fn update2(
-            &self,
-            _: f64,
-            _width: usize,
-            _height: usize,
-            _inframe1: &[u32],
-            _inframe2: &[u32],
-            _inframe3: &[u32],
-            _outframe: &mut [u32],
-        ) {
-            unreachable!()
-        }
+    fn update2(
+        &self,
+        _: f64,
+        _width: usize,
+        _height: usize,
+        _inframe1: &[u32],
+        _inframe2: &[u32],
+        _inframe3: &[u32],
+        _outframe: &mut [u32],
+    ) {
+        unreachable!()
     }
 }
 
@@ -90,25 +85,25 @@ fn plugin_info(plugin_type: frei0r_rs::PluginType) -> frei0r_rs::PluginInfo {
     }
 }
 
-impl plugin::PluginDefault for FrameServerPlugin<Source> {
+impl PluginDefault for FrameServerPlugin<Source> {
     fn info() -> frei0r_rs::PluginInfo {
         plugin_info(frei0r_rs::PluginType::Source)
     }
 }
 
-impl plugin::PluginDefault for FrameServerPlugin<Filter> {
+impl PluginDefault for FrameServerPlugin<Filter> {
     fn info() -> frei0r_rs::PluginInfo {
         plugin_info(frei0r_rs::PluginType::Filter)
     }
 }
 
-impl plugin::PluginDefault for FrameServerPlugin<Mixer2> {
+impl PluginDefault for FrameServerPlugin<Mixer2> {
     fn info() -> frei0r_rs::PluginInfo {
         plugin_info(frei0r_rs::PluginType::Mixer2)
     }
 }
 
-impl plugin::PluginDefault for FrameServerPlugin<Mixer3> {
+impl PluginDefault for FrameServerPlugin<Mixer3> {
     fn info() -> frei0r_rs::PluginInfo {
         plugin_info(frei0r_rs::PluginType::Mixer3)
     }
