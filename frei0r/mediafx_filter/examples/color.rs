a43d7a3deb6374c2b83b68a37d67bd53fa29d390
Andrew Wason <rectalogic@rectalogic.com>
2025-05-09 16:40:46 -0400

    add client examples and CI with ffmpeg

---
+++ frei0r/mediafx_filter/examples/color.rs
@@ -0,0 +1,29 @@
+use mediafx::frameserver::client;
+
+// XXX need to forward params to client
+const COLOR: [u8; 4] = [255, 0, 0, 255];
+
+fn source_frame(frame_client: client::FrameClient) -> client::FrameClient {
+    let size = frame_client.render_size();
+    let mut request = frame_client.request_render().unwrap();
+
+    let rendered_frame = request.get_rendered_frame_mut();
+    for dy in 0..size.height() {
+        for dx in 0..size.width() {
+            let pixel_offset = ((dy * size.width() + dx) * 4) as usize;
+            rendered_frame[pixel_offset] = COLOR[0];
+            rendered_frame[pixel_offset + 1] = COLOR[1];
+            rendered_frame[pixel_offset + 2] = COLOR[2];
+            rendered_frame[pixel_offset + 3] = COLOR[3];
+        }
+    }
+
+    request.render_complete().unwrap()
+}
+
+fn main() {
+    let mut frame_client = client::FrameClient::new().unwrap();
+    loop {
+        frame_client = source_frame(frame_client);
+    }
+}
---
+++ frei0r/mediafx_filter/examples/shift.rs
@@ -0,0 +1,34 @@
+use mediafx::frameserver::client;
+
+// XXX need to forward params to client
+const XSHIFT: f32 = 0.5;
+const YSHIFT: f32 = 0.25;
+
+fn filter_frame(frame_client: client::FrameClient) -> client::FrameClient {
+    let size = frame_client.render_size();
+    let mut request = frame_client.request_render().unwrap();
+
+    let xshift = (XSHIFT * size.width() as f32) as u32;
+    let yshift = (YSHIFT * size.height() as f32) as u32;
+    for dy in 0..size.height() {
+        for dx in 0..size.width() {
+            let sy = (dy + yshift) % size.height();
+            let sx = (dx + xshift) % size.width();
+            let dest_index = (dy * size.width() + dx) as usize * 4;
+            let source_index = (sy * size.width() + sx) as usize * 4;
+            for channel in 0..4 {
+                request.get_rendered_frame_mut()[dest_index + channel] =
+                    request.get_source_frame(0).unwrap()[source_index + channel];
+            }
+        }
+    }
+
+    request.render_complete().unwrap()
+}
+
+fn main() {
+    let mut frame_client = client::FrameClient::new().unwrap();
+    loop {
+        frame_client = filter_frame(frame_client);
+    }
+}
