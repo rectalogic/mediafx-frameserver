8f82168cf4dbb50d9753de62a62413985ca7e46f
Andrew Wason <rectalogic@rectalogic.com>
2025-07-03 10:20:06 -0400

    Redirect stdout to stderr on python

--- clients/mediafx_py/python/mediafx/__init__.py
+++ clients/mediafx_py/python/mediafx/__init__.py
@@ -1,4 +1,9 @@
 # Copyright (C) 2025 Andrew Wason
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import sys
+
 from ._mediafx import MediaFX as MediaFX
+
+# Redirect stdout so it doesn't interfere with the anonymous pipes MediaFX uses
+sys.stdout = sys.stderr
